eda30c8d773783f4db38dff7a262f3841ccb33ef
Chermod <297636096+chermod@users.noreply.github.com>
2026-07-27 20:51:25 +0100

    ref: tests to remove as

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,134 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { Parser, type ParserOptions } from "../classes/parser";
 import { ParserError } from "../classes/parser-error";
-import type {
-  BinaryExpression,
-  FunctionExpression,
-  LogicalExpression,
-  TernaryExpression,
-  UnaryExpression,
-  ValueExpression,
-} from "../types/expression";
+import type { LogicalExpression } from "../types/expression";
+import { getError } from "./utils";
 
-const withoutLocations = (expression: LogicalExpression): unknown => {
-  switch (expression.type) {
-    case "binary":
-      return {
-        type: "binary",
-        operator: expression.operator,
-        left: withoutLocations(expression.left),
-        right: withoutLocations(expression.right),
-      };
-    case "function":
-      return {
-        type: "function",
-        name: expression.name,
-        arguments: expression.arguments.map(withoutLocations),
-      };
-    case "ternary":
-      return {
-        type: "ternary",
-        left: withoutLocations(expression.left),
-        middle: withoutLocations(expression.middle),
-        right: withoutLocations(expression.right),
-      };
-    case "unary":
-      return {
-        type: "unary",
-        operator: expression.operator,
-        expression: withoutLocations(expression.expression),
-      };
-    case "value":
-      switch (expression.value.type) {
-        case "constant":
-          return {
-            type: "value",
-            value: {
-              type: "constant",
-              value: { ...expression.value.value },
-            },
-          };
-        case "list":
-          return {
-            type: "value",
-            value: {
-              type: "list",
-              items: expression.value.items.map(withoutLocations),
-            },
-          };
-        case "parameter":
-          return {
-            type: "value",
-            value: { type: "parameter", name: expression.value.name },
-          };
-      }
-  }
-};
-
-const getParserError = (action: () => unknown): ParserError => {
-  try {
-    action();
-  } catch (error) {
-    if (error instanceof ParserError) {
-      return error;
-    }
-    throw error;
-  }
-
-  throw new Error("Expected parser to throw ParserError");
-};
-
-// const getParseSafeError = (
-//   expression: string,
-// ): {
-//   type: "error";
-//   error: ParserError;
-// } => {
-//   const result = parseSafe(expression);
-//   if (result.type !== "error") {
-//     throw new Error("Expected parseSafe to fail");
-//   }
-
-//   return result;
-// };
-
-// const getIncompleteExpression = (
-//   errorResult: ReturnType<typeof getParseSafeError>,
-// ): LogicalExpression => {
-//   if (errorResult.error.incompleteExpression === null) {
-//     throw new Error("Expected incomplete expression to exist");
-//   }
-
-//   return errorResult.error.incompleteExpression;
-// };
-
-describe("parser", () => {
-  it("binary operator", () => {
-    expect.assertions(1);
-    expect(parse("1 + 2")).toMatchSnapshot();
-  });
-
-  it("unary operator", () => {
-    expect.assertions(1);
-    expect(parse("-2")).toMatchSnapshot();
-  });
-
-  it("ternary operator", () => {
-    expect.assertions(1);
-    expect(parse("false ? 1 : 2")).toMatchSnapshot();
-  });
-
-  it("function expression", () => {
-    expect.assertions(1);
-    expect(parse("MyFunctionName(1,2,false)")).toMatchSnapshot();
-  });
-
-  it("value expression", () => {
-    expect.assertions(1);
-    expect(parse("#11/06/2000#")).toMatchSnapshot();
-  });
-
+describe("valid expressions", () => {
   it.each([
+    "1+2",
+    "-2",
+    "false?1:2",
+    "MyFunctionName(1,2,false)",
+    "#11/06/2000#",
     "[1] < #11/06/2000#",
     "1 < 2",
     "1 > 2",
@@ -166,62 +48,16 @@ describe("parser", () => {
     "1 << 2",
     "1 >> 2",
     "Fun()",
-  ])("complex %s", (expr) => {
+    "x like y",
+    "x not in y",
+  ])("parses %s", (s) => {
     expect.assertions(1);
-    expect(withoutLocations(parse(expr))).toMatchSnapshot(expr);
+    expect(new Parser(s).parse()).toMatchSnapshot();
   });
+});
 
-  it("adds source locations to binary and grouped expressions", () => {
-    expect.assertions(4);
-
-    const binary = parse("1 + 2") as BinaryExpression;
-
-    expect(binary.location).toMatchObject({ offset: 0, extent: 5 });
-    expect(binary.left.location).toMatchObject({ offset: 0, extent: 1 });
-    expect(binary.right.location).toMatchObject({ offset: 4, extent: 1 });
-
-    const grouped = parse("(1 + 2)") as BinaryExpression;
-
-    expect(grouped.location).toMatchObject({ offset: 0, extent: 7 });
-  });
-
-  it("adds source locations to unary and ternary expressions", () => {
-    expect.assertions(3);
-
-    const unary = parse("-2") as UnaryExpression;
-
-    expect(unary.location).toMatchObject({ offset: 0, extent: 2 });
-    expect(unary.expression.location).toMatchObject({ offset: 1, extent: 1 });
-
-    const ternary = parse("false ? 1 : 2") as TernaryExpression;
-
-    expect(ternary.location).toMatchObject({ offset: 0, extent: 13 });
-  });
-
-  it("adds source locations to function calls and list expressions", () => {
-    expect.assertions(4);
-
-    const functionExpression = parse("Fun(1)") as FunctionExpression;
-
-    expect(functionExpression.location).toMatchObject({ offset: 0, extent: 6 });
-    expect(functionExpression.arguments[0]?.location).toMatchObject({
-      offset: 4,
-      extent: 1,
-    });
-
-    const list = parse("(1,2)") as ValueExpression;
-
-    expect(list.location).toMatchObject({ offset: 0, extent: 5 });
-    expect(
-      (list.value as { type: "list"; items: LogicalExpression[] }).items[1]
-        ?.location,
-    ).toMatchObject({
-      offset: 3,
-      extent: 1,
-    });
-  });
-
-  it("errors", () => {
+describe("progressive error messages", () => {
+  it("throws parser errors", () => {
     expect.assertions(2);
 
     const parserInvalidExpression = () => parse("(123 *)  10/");
@@ -233,38 +69,82 @@ describe("parser", () => {
   it("stops on first parse issue by default", () => {
     expect.assertions(1);
 
-    const parserError = getParserError(() => parse("(123 *)  10/"));
+    const parserError = getError(() => parse("(123 *)  10/"));
 
+    expect.assert(parserError instanceof ParserError);
     expect(parserError.errors).toHaveLength(1);
   });
 
   it("collects multiple issues when stopOnFirstError is false", () => {
     expect.assertions(1);
 
-    const parserError = getParserError(() =>
+    const parserError = getError(() =>
       parse("(123 *)  10/", { stopOnFirstError: false }),
     );
 
+    expect.assert(parserError instanceof ParserError);
     expect(parserError.errors.length).toBeGreaterThan(1);
   });
+});
 
-  // it("returns incomplete ast in parseSafe error case", () => {
-  // expect.assertions(2);
+describe("source tracking", () => {
+  it("adds source locations to binary and grouped expressions", () => {
+    expect.assertions(4);
 
-  // const result = getParseSafeError("1 2");
-  // const incompleteExpression = getIncompleteExpression(result);
+    const binary = parse("1 + 2");
 
-  // expect(result.error.incompleteExpression).not.toBeNull();
-  // expect(withoutLocations(incompleteExpression)).toStrictEqual({
-  //   type: "value",
-  //   value: {
-  //     type: "constant",
-  //     value: { type: "number", value: "1" },
-  //   },
-  // });
-  // });
+    expect.assert(binary.type === "binary");
+
+    expect(binary.location).toMatchObject({ offset: 0, extent: 5 });
+    expect(binary.left.location).toMatchObject({ offset: 0, extent: 1 });
+    expect(binary.right.location).toMatchObject({ offset: 4, extent: 1 });
+
+    const grouped = parse("(1 + 2)");
+
+    expect(grouped.location).toMatchObject({ offset: 0, extent: 7 });
+  });
+
+  it("adds source locations to unary and ternary expressions", () => {
+    expect.assertions(3);
+
+    const unary = parse("-2");
+
+    expect.assert(unary.type === "unary");
+
+    expect(unary.location).toMatchObject({ offset: 0, extent: 2 });
+    expect(unary.expression.location).toMatchObject({ offset: 1, extent: 1 });
+
+    const ternary = parse("false ? 1 : 2");
+
+    expect(ternary.location).toMatchObject({ offset: 0, extent: 13 });
+  });
+
+  it("adds source locations to function calls and list expressions", () => {
+    expect.assertions(4);
+
+    const functionExpression = parse("Fun(1)");
+
+    expect.assert(functionExpression.type === "function");
+
+    expect(functionExpression.location).toMatchObject({ offset: 0, extent: 6 });
+    expect(functionExpression.arguments[0]?.location).toMatchObject({
+      offset: 4,
+      extent: 1,
+    });
+
+    const list = parse("(1,2)");
+
+    expect.assert(list.type === "value");
+    expect(list.location).toMatchObject({ offset: 0, extent: 5 });
+
+    expect.assert(list.value.type === "list");
+    expect(list.value.items[1]?.location).toMatchObject({
+      offset: 3,
+      extent: 1,
+    });
+  });
 });
 
 const parse = (s: string, o?: ParserOptions): LogicalExpression => {
-  return new Parser(s, o).parse()
-} 
+  return new Parser(s, o).parse();
+};
